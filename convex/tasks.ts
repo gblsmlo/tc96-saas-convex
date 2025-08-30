77b2a881d060e8c58cfc3c743a3d61b6716071ee
Gabriel Melo <gblsmlo@gmail.com>
2025-08-30 16:03:27 -0300

    refactor: remove unused tasks query module

--- convex/tasks.ts
+++
@@ -1,8 +0,0 @@
-import { type QueryCtx, query } from './_generated/server'
-
-export const get = query({
-	args: {},
-	handler: async (ctx: QueryCtx) => {
-		return await ctx.db.query('tasks').collect()
-	},
-})
